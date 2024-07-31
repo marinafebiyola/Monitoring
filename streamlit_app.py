328dc02b4bfef2c4517faf1d80de3f5b0da5e95d
marinafebiyola <marinafebiyol@gmail.com>
2024-07-31 15:24:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-st.title("Streamlit dengan Backend Flask")
+st.title("Monitoring")
 
 if st.button("Dapatkan Data Acak"):
     response = requests.get("https://tricky-hats-decide.loca.lt/random-data")
