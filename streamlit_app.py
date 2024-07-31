c972bd41dc16ccf73b28c2a60388e0e82657626e
marinafebiyola <marinafebiyol@gmail.com>
2024-07-31 15:07:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,32 @@
 import streamlit as st
 import requests
+import time
 
 st.title("Monitoring System")
 
+# Fungsi untuk mendapatkan data acak dari backend
 def get_random_data():
     try:
-        response = requests.get("https://tender-turkeys-relate.loca.lt")
-        if response.status_code == 200:
-            return response.json()
-        else:
-            st.error(f"Server returned status code {response.status_code}")
-            return None
+        response = requests.get("https://stale-cities-warn.loca.lt/random-data")
+        data = response.json()
+        return data['data']
     except requests.RequestException as e:
         st.error(f"Error fetching data: {e}")
         return None
 
+# Tempat untuk menampilkan data acak
+data_placeholder = st.empty()
+
+# Menampilkan data acak secara terus-menerus
 st.write("Menampilkan data acak secara terus-menerus...")
 
 while True:
+    # Mendapatkan data acak
     data = get_random_data()
-    if data:
-        st.write(f"Data Acak: {data.get('data')}")
+    
+    # Menampilkan data acak di frontend
+    if data is not None:
+        data_placeholder.write(f"Data Acak: {data}")
+    
+    # Delay sebelum permintaan berikutnya (misalnya, setiap 5 detik)
     time.sleep(5)
