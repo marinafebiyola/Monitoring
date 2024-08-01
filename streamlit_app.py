5f57034ebe8950e648a6e3fee60d9c190318f246
marinafebiyola <marinafebiyol@gmail.com>
2024-08-01 15:24:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,15 @@ import time
 
 st.title("Monitoring")
 
-data_placeholder = st.empty()
-
 while True:
     try:
         response = requests.get("https://smooth-rice-greet.loca.lt/random-data", timeout=5)
         response.raise_for_status()
         data = response.json()
-        st.write(f"Data Acak: {data['data']}")
+        st.text(f"Data Acak: {data['data']}")
     except requests.exceptions.RequestException as e:
         st.error(f"Request gagal: {e}")
     except Exception as e:
         st.error(f"Terjadi kesalahan: {e}")
     
-    time.sleep(1)  # Interval waktu yang lebih stabil untuk pengiriman data
+    time.sleep(1)  # Interval waktu untuk mempercepat pemanggilan data
