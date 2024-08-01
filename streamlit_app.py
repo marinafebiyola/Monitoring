52aebf59bc6a71f3238a16655daa6334bf70087c
marinafebiyola <marinafebiyol@gmail.com>
2024-08-01 15:18:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,13 @@ data_placeholder = st.empty()
 
 while True:
     try:
-        response = requests.get("https://late-berries-cough.loca.lt/random-data", timeout=5)
+        response = requests.get("https://tangy-pears-cover.loca.lt/random-data", timeout=5)
         response.raise_for_status()
         data = response.json()
-        data_placeholder.text(f"Data Acak: {data['data']}")
+        st.write(f"Data Acak: {data['data']}")
     except requests.exceptions.RequestException as e:
         st.error(f"Request gagal: {e}")
     except Exception as e:
         st.error(f"Terjadi kesalahan: {e}")
     
-    time.sleep(5)  # Tunggu sebelum mencoba lagi
+    time.sleep(3)  # Interval waktu yang lebih stabil untuk pengiriman data
