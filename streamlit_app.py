3458241f7803dc793bee16b68aa43ab3aa1a2416
marinafebiyola <marinafebiyol@gmail.com>
2024-08-01 15:03:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import requests
 st.title("Monitoring")
 
 if st.button("Dapatkan Data Acak"):
-    response = requests.get("https://tricky-hats-decide.loca.lt/random-data")
+    response = requests.get("https://late-berries-cough.loca.lt/random-data")
     data = response.json()
     st.write(f"Data Acak: {data['data']}")
