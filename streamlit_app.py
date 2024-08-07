d1b037497aec91e4636aafd5f9be174f88d6f97c
marinafebiyola <marinafebiyol@gmail.com>
2024-08-07 14:07:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.title("Monitoring system")
 data_placeholder = st.empty()
 
 while True:
-    response = requests.get(" https://44cb-103-20-185-106.ngrok-free.app /random-data")
+    response = requests.get("http://localhost:5000/random-data")
     data = response.json()  # This line is causing the JSONDecodeError
     data_placeholder.write(f"Data Acak: {data['data']}")
     time.sleep(5)  # Adjust the sleep time as needed to control the update frequency
